7f001b4857ac6f2dd216c4fbd774b7d7f3997038
Yuriy Chamkoriyski <chamkoriyski@gmail.com>
2024-04-10 14:07:48 +0200

    Up to part 7 Dynamic routes

---
+++ src/app/products/page.tsx
@@ -0,0 +1,22 @@
+import React from 'react';
+
+const ProductList = () => {
+  return (
+    <div>
+      <h1>ProductList</h1>
+      <ul>
+        <li>
+          <h2>Product 1</h2>
+        </li>
+        <li>
+          <h2>Product 2</h2>
+        </li>
+        <li>
+          <h2>Product 3</h2>
+        </li>
+      </ul>
+    </div>
+  );
+};
+
+export default ProductList;
