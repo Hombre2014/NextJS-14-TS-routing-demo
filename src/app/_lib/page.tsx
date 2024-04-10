37280eefc4807db359e7b9e488f442d104364b98
Yuriy Chamkoriyski <chamkoriyski@gmail.com>
2024-04-10 17:25:03 +0200

    Upto topic 13 route groups

---
+++ src/app/_lib/page.tsx
@@ -0,0 +1,12 @@
+import React from 'react';
+
+const PrivateRoute = () => {
+  return (
+    <div>
+      <h1>Private Route</h1>
+      <p>You can not view this in the browser</p>
+    </div>
+  );
+};
+
+export default PrivateRoute;
